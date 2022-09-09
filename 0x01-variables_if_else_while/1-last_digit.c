4d3232704b838687f54dc3e3465fff47c5efa7a9
Marrockx <mariam.adekola10@gmail.com>
2022-09-09 15:04:14 -0700

    correct error in code-switched else if and else blocks

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,13 +23,13 @@ int main(void)
 	if (lastDigit > 5)
 	{
 		printf("Last digit of %i is %i and is greater than 5\n", n, lastDigit);
-	} else if (n == 0)
-	{
-		printf("Last digit of %i is %i and is 0\n", n, lastDigit);
-	} else
+	} else if ((lastDigit < 6) & (lastDigit != 0))
 	{
 		printf("Last digit of %i is %i and is less than 6 and not 0\n",
 				n, lastDigit);
+	} else
+	{
+		printf("Last digit of %i is %i and is 0\n", n, lastDigit);
 	}
 	return (0);
 }
