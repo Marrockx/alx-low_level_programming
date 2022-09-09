a7d6ce5f22c79f25707d90b20621636d4ee12b5a
Marrockx <mariam.adekola10@gmail.com>
2022-09-09 14:36:45 -0700

    fix betty styling errors

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,10 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 
-/*
+/**
 * main - entry point for c program
 *
-* Description: Given a randome number, print it and 
+* Description: Given a random number, print it and
 * check if random number is positive, negative or zero and print output
 *
 * Return: 0
@@ -18,13 +18,13 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%i is positive\n",n);
+		printf("%i is positive\n", n);
 	} else if (n < 0)
 	{
-		printf("%i is negative\n",n);
+		printf("%i is negative\n", n);
 	} else
 	{
-		printf("%i is zero",n);
+		printf("%i is zero\n", n);
 	}
 	return (0);
 }
