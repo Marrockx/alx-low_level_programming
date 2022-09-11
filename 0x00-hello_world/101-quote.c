e61569f5eb3b748f70b87eb872839e5d1bce4441
Marrockx <mariam.adekola10@gmail.com>
2022-09-11 15:47:39 -0700

    write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
 /**
 * main - entry point
+*
+* Description: Program to write out statement
 *
 * Return: 1
 */
 int main(void)
 {
-	printf("and that piece of art is useful""\ - Dora Korpar, 2015-10-19\n");
+	int length;
+
+	length = strlen("and that piece of art is useful - Dora Korpar,
+			2015-10-19\n");
+	write(1,
+		"and that piece of art is useful - Dora Korpar, 2015-10-19\n",
+		length);
 	return (1);
 }
