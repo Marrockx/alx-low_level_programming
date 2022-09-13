ca3ddfc5a66e24460a383624b34c0a768a8de48b
Marrockx <mariam.adekola10@gmail.com>
2022-09-13 15:16:38 -0700

    print lowercase alphabet using _putchar twice

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print lowercase alphabet characters
+ *
+ * Description: using _putchar twice
+ *
+ * Return: (void) No return type
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter = letter + 1;	
+	}	
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - call print alphabet function
+ *
+ * Return: 0 (success)
+ *
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
