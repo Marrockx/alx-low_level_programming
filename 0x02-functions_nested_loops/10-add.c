af03ccb3e5a3acb913b78e2fb9672c63e0dd51e0
Marrockx <mariam.adekola10@gmail.com>
2022-09-14 12:14:26 -0700

    addd two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - function to add two numbers
+ * @num1: first number to be added
+ * @num2: second number to be added
+ * Return: (int) result of addition
+ */
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+	return (sum);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,9 +20,7 @@ void times_table(void)
 		{
 			_putchar(',');
 			_putchar(' ');
-			
 			product = i * multiplier;
-			
 			if (product < 10)
 			{
 				_putchar(' ');
@@ -31,7 +29,7 @@ void times_table(void)
 			{
 				_putchar(product / 10 + '0');
 			}
-			
+
 			_putchar(product % 10 + '0');
 			multiplier++;
 		}
