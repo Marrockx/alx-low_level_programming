0876ca692834cf4ab505ddb935c85945ba44010a
Marrockx <mariam.adekola10@gmail.com>
2022-09-15 19:54:13 -0700

    multiplication task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - function multiplies two given numbers
+ * @a: first number, the multiplicand
+ * @b: second number, the multiplier
+ * Description: prints result of multiplication (product)
+ * Return: product value
+ */
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
