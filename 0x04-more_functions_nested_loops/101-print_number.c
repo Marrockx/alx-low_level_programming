522deda399cd1a1083676832665b728356ac701c
Marrockx <mariam.adekola10@gmail.com>
2022-09-17 06:42:30 -0700

    print more num

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -23,26 +23,9 @@ void print_number(int n)
 		n = n * -1;
 		y = n; /* to get the negative value of unsigned */
 	}
-	if (n < 10)
+	if (n / 10)
 	{
-		_putchar(y + '0');
+		print_number(n / 10);
 	}
-	else if (n < 100)
-	{
-		_putchar(y / 10 + '0');
-		_putchar(y % 10 + '0');
-	}
-	else if (n < 1000)
-	{
-		_putchar((y / 100) + '0');
-		_putchar((y % 100) / 10 + '0');
-		_putchar(y % 10 + '0');
-	}
-	else
-	{
-		_putchar(y / 1000 + '0');
-		_putchar((y % 1000) / 100 + '0');
-		_putchar((y % 100) / 10 + '0');
-		_putchar(y % 10 + '0');
-	}
+	_putchar(y % 10 + '0');
 }
