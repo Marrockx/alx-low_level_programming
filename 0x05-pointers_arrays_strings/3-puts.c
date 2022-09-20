db50da7acf870ec82fa727fd1f1b8a1a1e235e4a
Marrockx <mariam.adekola10@gmail.com>
2022-09-19 19:27:50 -0700

    print string to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string,
+ * followed by a new line, to stdout
+ * @str: string to be printed to stdout
+ *
+ * Return: void type
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,5 +3,8 @@
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void reset_to_98(int *n);
+void _puts(char *str);
+
+int _putchar(char c);
 
 #endif /* MAIN_H */
