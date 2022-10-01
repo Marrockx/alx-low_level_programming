4ad1561117d3dc1877d07f59ba08a1b9a1bc71a2
Marrockx <mariam.adekola10@gmail.com>
2022-10-01 01:51:22 +0100

    first 98 fib nos

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,9 +2,8 @@
 
 /**
  * main - starting point of program
- * Description: program to print sum of even Fibonacci numbers
- * up to a fib value, not exceeding 4,000,000.
- * starting from 1 and 2 followed by a new line
+ * Description: program to print first 98 Fibonacci numbers
+ * starting with 1 and 2 followed by a new line
  *
  * Return: Always 0
  */
@@ -14,12 +13,12 @@ int main(void)
 	unsigned long int fib1 = 1, fib2 = 1, fib_sum;
 	int i = 0;
 
-	while (i < 98)
+	while (i < 100)
 	{
 		fib_sum = fib1 + fib2;
 
 		printf("%lu", fib2);
-		if (i != 97)
+		if (i != 99)
 		{
 			printf(", ");
 		}
