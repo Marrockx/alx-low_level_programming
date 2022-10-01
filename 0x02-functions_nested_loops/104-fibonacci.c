0bf2a6691a27c5df1e9f5d7e47fd4434afb14aed
Marrockx <mariam.adekola10@gmail.com>
2022-10-01 02:05:47 +0100

    first 98 fib nos

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,14 +10,14 @@
 
 int main(void)
 {
-	unsigned long long int fib1 = 1, fib2 = 1, fib_sum;
+	unsigned long int fib1 = 1, fib2 = 1, fib_sum;
 	int i = 0;
 
 	while (i < 98)
 	{
 		fib_sum = fib1 + fib2;
 
-		printf("%llu", fib2);
+		printf("%lu", fib2);
 		if (i != 97)
 		{
 			printf(", ");
