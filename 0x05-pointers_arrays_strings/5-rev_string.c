f93468f1bdcbb7b5699dfecc2f7959254070cc65
Marrockx <mariam.adekola10@gmail.com>
2022-10-05 20:16:04 +0100

    rev

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,11 +10,19 @@
 
 void rev_string(char *s)
 {
-	int i;
-	char size = sizeof(s);
+	int i, c, l;
+	char h;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		printf("%c", s[size - i - 1]);
+		;
+	}
+	
+	l = i;
+	for (i--; c = 0; c < l / 2; i--; c++)
+	{
+		h = s[c];
+		s[c] = s[i];
+		s[i] = h;	
 	}
 }
