611a742c4bd3365262bc6d7f9feaef94a8cfa71c
Marrockx <mariam.adekola10@gmail.com>
2022-10-09 00:28:36 +0100

    encode

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,30 +1,42 @@
 #include "main.h"
 
 /**
- * leet - encode a string into 1337
- * @s: string
- * Return: encoded string `s`
+ * leet - encodes strings
+ * @str: character to encode
+ * Return: char type
  */
 
-char *leet(char *s)
+char *leet(char *str)
 {
 	int i;
 
-	i = 0;
-	while (s[i] != '\0')
+	for (i = 0; str[i] != '0'; i++)
 	{
-		while (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		while (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		while (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		while (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		while (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
-		i++;
+		while (str[i] == 'A' || str[i] == 'a')
+		{
+			str[i] = '4';
+		}
+
+		while (str[i] == 'E' || str[i] == 'e')
+		{
+			str[i] = '3';
+		}
+
+		while (str[i] == 'O' || str[i] == 'o')
+		{
+			str[i] = '0';
+		}
+
+		while (str[i] == 'T' || str[i] == 't')
+		{
+			str[i] = '7';
+		}
+
+		while (str[i] == 'L' || str[i] == 'l')
+		{
+			str[i] = '1';
+		}
 	}
-	
-	return (s);
+
+	return (str);
 }
