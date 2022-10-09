45e4789264f30220e02e0338f63796553a6b66e6
Marrockx <mariam.adekola10@gmail.com>
2022-10-09 01:50:23 +0100

    ok

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -18,4 +18,12 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
+	int len1 = 0, len2 = 0, lenS = 0;
+	
+	while (*(n1 + len1++))
+		;
+	while (*(n2 + len2++))
+		;
+	printf("%d, %d\n", len1, len2);
+	return (r);
 }
