e24fa0f156fc2f1f4146d71a65c46357331c6f91
Marrockx <mariam.adekola10@gmail.com>
2022-10-10 13:41:28 +0100

    strch

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 #define MAIN_H
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 void set_string(char **s, char *to);
 
