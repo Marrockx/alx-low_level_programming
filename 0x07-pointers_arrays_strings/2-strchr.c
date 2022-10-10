28356258c47da41a034c569495559b2c10513d2e
Marrockx <mariam.adekola10@gmail.com>
2022-10-10 13:48:22 +0100

    strch

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,11 +12,13 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i]; i++)
 	{
 		if (s[i] == c)
-			return (&s[i]);
+			return (s + i);
 	}
 
+	if (s[i] == c)
+		return (s + i);
 	return (0);
 }
