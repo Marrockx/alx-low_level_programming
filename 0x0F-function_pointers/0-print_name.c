ddadf3b14f89be086f115452fe566250d31bf13b
Marrockx <mariam.adekola10@gmail.com>
2022-10-12 19:34:58 +0100

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name;
+ * @name: name to print of char * type
+ * @f: function pointed to, which takes in char * argument and returns void
+ * Return: void type
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,11 @@
+# 0x0F. C - Function pointers
+
+## Tasks
+### Mandatory
+- [x] **[0. What's my name](0-print_name.c)**
+- [x] **[1. If you spend too much time thinking about a thing, you'll never get it done](1-array_iterator.c)**
+### Advanced
+
+## Resource Links
+- []()
+## My Thoughts
