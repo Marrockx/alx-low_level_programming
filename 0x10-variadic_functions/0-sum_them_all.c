57c2b68117059de4311c5fc8f7b19d9ca3ffa512
Marrockx <mariam.adekola10@gmail.com>
2022-10-13 16:59:49 +0100

    sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,23 +9,20 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	unsigned int i;
+	int readArg, sum = 0;
+	va_list args;
+
 	if (n == 0)
 		return (0);
 
-	va_list args;
-
-	unsigned int i;
-	int readArg, sum = 0;
-
 	va_start(args, n);
 
-	while (i < n)
+	for (i = 0; i < n; i++)
 	{
 		readArg = va_arg(args, int);
 
 		sum += readArg;
-
-		i++;
 	}
 
 	va_end(args);
